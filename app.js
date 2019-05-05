4ff416e0cbcff57e92c5c163db64045228b073e3
Leo Gassman <lgassman@gmail.com>
2019-05-05 10:33:41 -0300

    modularizacion

---
+++ app.js
@@ -0,0 +1,2 @@
+server = require("./server")
+server.init();
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var http = require("http");
 
-console.log("Loading server");
+function init() {
+  console.log("Loading server");
 
-http.createServer(function(request, response) {
-  console.log("Request received");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
-  response.end();
-}).listen(8888);
+  http
+    .createServer(function(request, response) {
+      console.log("Request received");
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("Hola Mundo");
+      response.end();
+    })
+    .listen(8888);
+}
+
+exports.init = init;
