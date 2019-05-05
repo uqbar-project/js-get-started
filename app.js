21c333c098b5d88d6735b88b572a7eceed63bd82
Leo Gassman <lgassman@gmail.com>
2019-05-05 13:15:03 -0300

    using mongodb

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 server = require("./server")
-Producto = require ("./src/producto")
-Cliente = require ("./src/cliente")
-Home = require("./src/memoryHome")
 
-productoHome = new Home("productos")
-
-productoHome.insert(new Producto("alfajor", 20))
-productoHome.insert(new Producto("chocolate", 30))
-
-clienteHome = new Home("clientes")
-
-clienteHome.insert(new Cliente("pepito", "calle falsa 123"))
-clienteHome.insert(new Cliente("fulanito", "calle san martín s/n"))
-
-server.register(productoHome)
-server.register(clienteHome)
-server.init();
+mongoConnection = require("./src/mongo/mongoConnection")
+Home = require("./src/mongo/mongoHome")
+mongoConnection.connect( (db) => {
+    productoHome = new Home("productos", db)
+    clienteHome = new Home("clientes", db)    
+    server.register(productoHome)
+    server.register(clienteHome)
+    server.init();
+})
