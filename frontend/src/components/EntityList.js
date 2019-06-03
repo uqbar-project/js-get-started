63345aec632bac9a2592c60b00c74f76ccf42b1b
Leo Gassman <lgassman@gmail.com>
2019-06-03 18:49:36 -0300

    tabla de entitades

--- frontend/src/components/EntityList.js
+++ frontend/src/components/EntityList.js
@@ -16,28 +16,19 @@ class EntityList extends React.Component {
     render() {
       if(this.state.entities.length > 0) {
         //uso el primero para conocer los atributos
-        //first = this.state.entities[0];
-
+      var columns = Object.entries(this.state.entities[0]).map(entry => entry[0])
       return (
         <div className="EntityList">
-<table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.entities.map((object, index) => {
-              return (
-                <tr key={object._id}>
-                  <td>{object._id}</td>
-                  <td>{object.nombre}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+          <table className="table">
+            <thead>
+              <tr>
+                {this.renderHeaders(columns)}
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderRows(columns)}
+            </tbody>
+          </table>
         </div>
       );
     }
@@ -45,7 +36,33 @@ class EntityList extends React.Component {
       return (<p> Cargando {this.props.entity} </p>);
     }
     }
+
+    renderHeaders(columns) {
+      return columns.map((col, index) => {
+        return (
+            <th>{col}</th>
+        );
+      })      
+    }
+
+    renderRows(columns) {
+      return this.state.entities.map((object, index) => {
+        return (
+          <tr key={object._id}>
+            {this.renderRow(object, columns)}
+          </tr>
+        );
+      })
+    }
+
+    renderRow(object, columns,) {
+      return columns.map((attName, index) => {
+          return (<td>{object[attName]}</td>);
+      });
+    }
   }
+
+
 
   export default EntityList
   
